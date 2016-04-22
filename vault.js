21f83eeb113feb3aae585bd3e6502b9fccf6615a
BBishop <brad.bishop68@gmail.com>
2016-04-22 00:47:46 -1000

    Fixes getValue function

--- vault.js
+++ vault.js
@@ -1,4 +1,25 @@
-'use strict';
+
 module.exports = function() {
-  
+'use strict';
+  var vault = {};
+  return {
+   getValue: function (key) {
+    if (key === undefined) {
+      return null;
+    }
+    else if (key === vault.vault_key) {
+      return vault.vault_value;
+    }
+    else if (key !== vault.vault_key) {
+      return null;
+    }
+  },
+   setValue: function (key, value) {
+     vault.vault_key = key;
+     vault.vault_value = value;
+
+   }
+  };
 };
+
+
